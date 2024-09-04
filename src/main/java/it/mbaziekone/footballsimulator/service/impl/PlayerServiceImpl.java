9a70e27472f65cbef95cca103267461897dee2e1
M'bazie Kone <mbazie89@gmail.com>
2024-09-04 23:20:41 +0200

    Updates

---
+++ src/main/java/it/mbaziekone/footballsimulator/service/impl/PlayerServiceImpl.java
@@ -0,0 +1,29 @@
+package it.mbaziekone.footballsimulator.service.impl;
+
+import java.util.List;
+
+import it.mbaziekone.footballsimulator.model.Player;
+import it.mbaziekone.footballsimulator.model.Team;
+import it.mbaziekone.footballsimulator.service.PlayerService;
+
+public class PlayerServiceImpl implements PlayerService {
+
+	@Override
+	public Player getPlayerById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Player> getPlayersByTeam(Team team) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void savePlayer(Player player) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
