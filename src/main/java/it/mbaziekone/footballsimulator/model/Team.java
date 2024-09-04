05f7b9b3026366878d31db217f81178d874eb0f7
M'bazie Kone <mbazie89@gmail.com>
2024-09-04 22:16:21 +0200

    First commit

--- src/main/java/it/mbaziekone/footballsimulator/model/Team.java
+++ src/main/java/it/mbaziekone/footballsimulator/model/Team.java
@@ -1,10 +1,21 @@
 package it.mbaziekone.footballsimulator.model;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "teams")
-public class Team {
+public class Team implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
 }
