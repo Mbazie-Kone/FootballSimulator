80c9521dd4abefaa5828dae3ba8b14d46c5b1218
M'bazie Kone <mbazie89@gmail.com>
2024-09-04 23:36:47 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/controller/PlayerController.java
+++ src/main/java/it/mbaziekone/footballsimulator/controller/PlayerController.java
@@ -1,5 +1,10 @@
 package it.mbaziekone.footballsimulator.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/players")
 public class PlayerController {
 
 }
