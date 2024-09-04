505dbcd7f0c788d80b92e4ff1483dc50d5dcf9b0
M'bazie Kone <mbazie89@gmail.com>
2024-09-04 23:17:18 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/service/PlayerService.java
+++ src/main/java/it/mbaziekone/footballsimulator/service/PlayerService.java
@@ -1,8 +1,13 @@
 package it.mbaziekone.footballsimulator.service;
 
+import java.util.List;
+
 import it.mbaziekone.footballsimulator.model.Player;
+import it.mbaziekone.footballsimulator.model.Team;
 
 public interface PlayerService {
 	
 	public Player getPlayerById(Long id);
+	
+	public List<Player> getPlayersByTeam(Team team);
 }
