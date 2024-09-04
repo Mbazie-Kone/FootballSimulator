656af9b0231c8e1cfcf008209afa63bb9e758918
M'bazie Kone <mbazie89@gmail.com>
2024-09-04 22:55:33 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/model/Team.java
+++ src/main/java/it/mbaziekone/footballsimulator/model/Team.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -17,5 +19,11 @@ public class Team implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	private String name;
+	
+	@ManyToOne
+	@JoinColumn(name = "nation_id")
+	private Nation nation;
 
 }
