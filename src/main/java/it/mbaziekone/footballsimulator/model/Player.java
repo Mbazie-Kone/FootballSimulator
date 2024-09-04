58803ad40095084f8493111fa63c2becedeee314
M'bazie Kone <mbazie89@gmail.com>
2024-09-04 22:13:20 +0200

    First commit

--- src/main/java/it/mbaziekone/footballsimulator/model/Player.java
+++ src/main/java/it/mbaziekone/footballsimulator/model/Player.java
@@ -1,5 +1,17 @@
 package it.mbaziekone.footballsimulator.model;
 
-public class Player {
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "palyers")
+public class Player implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	
 
 }
