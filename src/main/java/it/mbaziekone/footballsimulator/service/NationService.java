4849fb4bddd833424835194dac8166a8c16364bf
M'bazie Kone <mbazie89@gmail.com>
2024-09-05 00:20:36 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/service/NationService.java
+++ src/main/java/it/mbaziekone/footballsimulator/service/NationService.java
@@ -1,9 +1,13 @@
 package it.mbaziekone.footballsimulator.service;
 
+import java.util.List;
+
 import it.mbaziekone.footballsimulator.model.Nation;
 
 public interface NationService {
 
 	Nation getNationById(Long nationId);
+	
+	List<Nation> getAllNations();
 
 }
--- src/main/java/it/mbaziekone/footballsimulator/service/impl/NationServiceImpl.java
+++ src/main/java/it/mbaziekone/footballsimulator/service/impl/NationServiceImpl.java
@@ -1,13 +1,24 @@
 package it.mbaziekone.footballsimulator.service.impl;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
 import it.mbaziekone.footballsimulator.model.Nation;
 import it.mbaziekone.footballsimulator.service.NationService;
 
+@Service
 public class NationServiceImpl implements NationService {
 
 	@Override
 	public Nation getNationById(Long nationId) {
-		// TODO Auto-generated method stub
+		
+		return null;
+	}
+
+	@Override
+	public List<Nation> getAllNations() {
+		
 		return null;
 	}
 
