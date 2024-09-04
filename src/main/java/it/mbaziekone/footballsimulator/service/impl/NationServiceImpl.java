ec8ed9a2a43386dbf1d0178af95224e1995d7721
M'bazie Kone <mbazie89@gmail.com>
2024-09-05 00:22:27 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/service/impl/NationServiceImpl.java
+++ src/main/java/it/mbaziekone/footballsimulator/service/impl/NationServiceImpl.java
@@ -2,24 +2,29 @@ package it.mbaziekone.footballsimulator.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import it.mbaziekone.footballsimulator.model.Nation;
+import it.mbaziekone.footballsimulator.repository.NationRepository;
 import it.mbaziekone.footballsimulator.service.NationService;
 
 @Service
 public class NationServiceImpl implements NationService {
+	
+	@Autowired
+	private NationRepository nationRepository;
 
 	@Override
 	public Nation getNationById(Long nationId) {
 		
-		return null;
+		return nationRepository.findById(nationId).orElse(null);
 	}
 
 	@Override
 	public List<Nation> getAllNations() {
 		
-		return null;
+		return nationRepository.findAll();
 	}
 
 }
