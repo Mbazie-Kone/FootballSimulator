26c9b733aaa7e974a56519c2bf34639bb317ed73
M'bazie Kone <mbazie89@gmail.com>
2024-09-04 23:41:50 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/controller/PlayerController.java
+++ src/main/java/it/mbaziekone/footballsimulator/controller/PlayerController.java
@@ -1,10 +1,32 @@
 package it.mbaziekone.footballsimulator.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import it.mbaziekone.footballsimulator.model.Player;
+import it.mbaziekone.footballsimulator.service.PlayerService;
 
 @RestController
 @RequestMapping("/api/players")
 public class PlayerController {
+	
+	@Autowired
+	private PlayerService playerService;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Player> getPlayer(@PathVariable Long id) {
+		
+		Player player = playerService.getPlayerById(id);
+		if(player == null) {
+
+			return ResponseEntity.notFound().build();		
+		}
+		
+		return ResponseEntity.ok(player);
+	}
 
 }
