a7af0684b4044fa61753b5cf877a4c53d4169f43
M'bazie Kone <mbazie89@gmail.com>
2024-09-05 00:18:16 +0200

    Updates

---
+++ src/main/java/it/mbaziekone/footballsimulator/service/impl/NationServiceImpl.java
@@ -0,0 +1,14 @@
+package it.mbaziekone.footballsimulator.service.impl;
+
+import it.mbaziekone.footballsimulator.model.Nation;
+import it.mbaziekone.footballsimulator.service.NationService;
+
+public class NationServiceImpl implements NationService {
+
+	@Override
+	public Nation getNationById(Long nationId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
