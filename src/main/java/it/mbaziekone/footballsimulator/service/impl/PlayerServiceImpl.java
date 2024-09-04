638b457c6bb60a4879ef878a48d93039dd313bd6
M'bazie Kone <mbazie89@gmail.com>
2024-09-04 23:22:18 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/service/impl/PlayerServiceImpl.java
+++ src/main/java/it/mbaziekone/footballsimulator/service/impl/PlayerServiceImpl.java
@@ -2,14 +2,19 @@ package it.mbaziekone.footballsimulator.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import it.mbaziekone.footballsimulator.model.Player;
 import it.mbaziekone.footballsimulator.model.Team;
+import it.mbaziekone.footballsimulator.repository.PlayerRepository;
 import it.mbaziekone.footballsimulator.service.PlayerService;
 
 @Service
 public class PlayerServiceImpl implements PlayerService {
+	
+	@Autowired
+	private PlayerRepository playerRepository;
 
 	@Override
 	public Player getPlayerById(Long id) {
