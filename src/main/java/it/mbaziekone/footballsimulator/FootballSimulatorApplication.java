920ddefc3438d68ae0b81db9bcd81e3740a68ce3
M'bazie Kone <mbazie89@gmail.com>
2024-09-04 21:49:41 +0200

    First commit

---
+++ src/main/java/it/mbaziekone/footballsimulator/FootballSimulatorApplication.java
@@ -0,0 +1,13 @@
+package it.mbaziekone.footballsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/mbaziekone/footballsimulator/FootballSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package it.mbaziekone.footballsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
