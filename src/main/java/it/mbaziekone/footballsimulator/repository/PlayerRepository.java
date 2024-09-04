7e3df8fc5a5226fe054115e3473836680ba45277
M'bazie Kone <mbazie89@gmail.com>
2024-09-04 23:11:09 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/repository/PlayerRepository.java
+++ src/main/java/it/mbaziekone/footballsimulator/repository/PlayerRepository.java
@@ -1,11 +1,15 @@
 package it.mbaziekone.footballsimulator.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import it.mbaziekone.footballsimulator.model.Player;
+import it.mbaziekone.footballsimulator.model.Team;
 
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
-
+	
+	List<Player> findByTeam(Team team);
 }
