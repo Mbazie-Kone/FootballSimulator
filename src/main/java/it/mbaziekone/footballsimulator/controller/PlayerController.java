11ac852ef2189f9d8fa7d59538310b3c7f08230b
M'bazie Kone <mbazie89@gmail.com>
2024-09-04 23:45:32 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/controller/PlayerController.java
+++ src/main/java/it/mbaziekone/footballsimulator/controller/PlayerController.java
@@ -1,5 +1,7 @@
 package it.mbaziekone.footballsimulator.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.mbaziekone.footballsimulator.model.Player;
+import it.mbaziekone.footballsimulator.model.Team;
 import it.mbaziekone.footballsimulator.service.PlayerService;
 
 @RestController
@@ -27,6 +30,13 @@ public class PlayerController {
 		}
 		
 		return ResponseEntity.ok(player);
+	}
+	
+	
+	public List<Player> getPlayersByteam(@PathVariable Long teamId) {
+		
+		Team team = new Team();
+		team.setId(teamId);
 	}
 
 }
--- src/main/java/it/mbaziekone/footballsimulator/model/Team.java
+++ src/main/java/it/mbaziekone/footballsimulator/model/Team.java
@@ -31,6 +31,14 @@ public class Team implements Serializable {
 	
 	@OneToMany(mappedBy = "team")
 	private List<Player> players = new ArrayList<>();
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getName() {
 		return name;
