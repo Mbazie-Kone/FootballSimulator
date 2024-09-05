a629eb890b239b4e3e631a7278fa25b9a592a79a
M'bazie Kone <mbazie89@gmail.com>
2024-09-05 02:13:48 +0200

    Updates

---
+++ src/main/java/it/mbaziekone/footballsimulator/game/MainUI.java
@@ -0,0 +1,14 @@
+package it.mbaziekone.footballsimulator.game;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainUI extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		primaryStage.setTitle("Football Simulator");
+	}
+
+}
