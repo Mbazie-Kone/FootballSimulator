b4410cfd4d79bda7182c19af4063982f88efc6c5
M'bazie Kone <mbazie89@gmail.com>
2024-09-05 03:15:04 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/game/MessagesView.java
+++ src/main/java/it/mbaziekone/footballsimulator/game/MessagesView.java
@@ -1,5 +1,18 @@
 package it.mbaziekone.footballsimulator.game;
 
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
 public class MessagesView {
+	
+	public static VBox getMessagesView() {
+		
+		VBox layout = new VBox();
+		
+		Label message1 = new Label("Welcome to Football Simulator!");
+		
+		Label message2 = new Label("Your team has a new training session.");
+	
+	}
 
 }
