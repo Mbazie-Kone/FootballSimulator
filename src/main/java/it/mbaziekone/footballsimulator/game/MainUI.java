06922d1d2604add93e45e57801d01fc14fe42405
M'bazie Kone <mbazie89@gmail.com>
2024-09-05 02:17:58 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/game/MainUI.java
+++ src/main/java/it/mbaziekone/footballsimulator/game/MainUI.java
@@ -1,6 +1,10 @@
 package it.mbaziekone.footballsimulator.game;
 
+import javax.swing.plaf.basic.BasicTabbedPaneUI.TabbedPaneLayout;
+
 import javafx.application.Application;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainUI extends Application {
@@ -9,6 +13,10 @@ public class MainUI extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		
 		primaryStage.setTitle("Football Simulator");
+		
+		BorderPane mainLayout = new BorderPane();
+		
+		TabPane tabPane = new TabPane();
 	}
 
 }
