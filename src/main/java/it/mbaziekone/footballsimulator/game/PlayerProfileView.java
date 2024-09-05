8939cea48ff7b7f88cef10b65848da9cb481cafb
M'bazie Kone <mbazie89@gmail.com>
2024-09-05 03:09:41 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/game/PlayerProfileView.java
+++ src/main/java/it/mbaziekone/footballsimulator/game/PlayerProfileView.java
@@ -1,10 +1,17 @@
 package it.mbaziekone.footballsimulator.game;
 
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 public class PlayerProfileView {
 	
 	public static VBox getPlayerProfileView() {
 		
+		VBox layout = new VBox();
+		
+		Label playerName = new Label();
+		Label playerNPosition = new Label();
+		Label playerTeam = new Label();
+		Label playerSalary = new Label();
 	}
 }
