38f446a73e9152b35c216cf0cf81a469624e31ef
M'bazie Kone <mbazie89@gmail.com>
2024-09-06 01:28:51 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/game/MainMenu.java
+++ src/main/java/it/mbaziekone/footballsimulator/game/MainMenu.java
@@ -1,13 +1,15 @@
 package it.mbaziekone.footballsimulator.game;
 
 import javafx.application.Application;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainMenu extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		// TODO Auto-generated method stub
+		
+		VBox menuLayout = new VBox(20);
 		
 	}
 
