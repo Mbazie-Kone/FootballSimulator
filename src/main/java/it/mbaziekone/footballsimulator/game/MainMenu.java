07068e0f04f99663c9fe74040df50cb945f510b5
M'bazie Kone <mbazie89@gmail.com>
2024-09-06 01:27:44 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/game/MainMenu.java
+++ src/main/java/it/mbaziekone/footballsimulator/game/MainMenu.java
@@ -1,7 +1,14 @@
 package it.mbaziekone.footballsimulator.game;
 
 import javafx.application.Application;
+import javafx.stage.Stage;
 
 public class MainMenu extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
