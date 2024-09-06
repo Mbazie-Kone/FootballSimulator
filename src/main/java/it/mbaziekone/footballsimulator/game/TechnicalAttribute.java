dfd2eeb26b92bc50c6189c81f6d59b08eded740e
M'bazie Kone <mbazie89@gmail.com>
2024-09-06 03:03:49 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/game/TechnicalAttribute.java
+++ src/main/java/it/mbaziekone/footballsimulator/game/TechnicalAttribute.java
@@ -1,5 +1,23 @@
 package it.mbaziekone.footballsimulator.game;
 
 public enum TechnicalAttribute {
+	
+	HEADING("Heading"),
+    SHOOTING("Shooting"),
+    PASSING("Passing"),
+    TECHNIQUE("Technique"),
+    DRIBBLING("Dribbling"),
+    FINISHING("Finishing"),
+    MARKING("Marking"),
+    TACKLING("Tackling");
 
+    private final String attributeName;
+
+    TechnicalAttribute(String attributeName) {
+        this.attributeName = attributeName;
+    }
+
+    public String getAttributeName() {
+        return attributeName;
+    }
 }
