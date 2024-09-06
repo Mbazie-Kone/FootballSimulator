7850c77c0d8eed3911562527eb2f552ef2e7cb53
M'bazie Kone <mbazie89@gmail.com>
2024-09-06 02:17:48 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/game/SettingsMenu.java
+++ src/main/java/it/mbaziekone/footballsimulator/game/SettingsMenu.java
@@ -11,15 +11,19 @@ public class SettingsMenu {
 	
 	public void start(Stage primaryStage) {
 		
+		// Layout delle impostazioni
 		VBox settingsLayout = new VBox(20);
 		settingsLayout.setAlignment(Pos.CENTER);
 		
-		
+		// Slider per il volume della musica
 		Label volumeLabel = new Label("Music volume:");
 		Slider volumeSlider = new Slider(0, 100, 50); // Min: 0, Max: 100, Default: 50
 		
-		
+		// Pulsante per tornare al menù principale
 		Button backButton = new Button("Return to menu");
 		backButton.setOnAction(e -> returnToMenu(primaryStage));
+		
+		
+		settingsLayout.getChildren().addAll(volumeLabel, volumeSlider, backButton);
 	}
 }
