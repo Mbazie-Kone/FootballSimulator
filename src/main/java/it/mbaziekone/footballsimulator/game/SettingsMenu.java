6b7852b36cd1e946f4e14e0fa8f12c9214eda799
M'bazie Kone <mbazie89@gmail.com>
2024-09-06 02:10:38 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/game/SettingsMenu.java
+++ src/main/java/it/mbaziekone/footballsimulator/game/SettingsMenu.java
@@ -1,10 +1,12 @@
 package it.mbaziekone.footballsimulator.game;
 
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class SettingsMenu {
 	
 	public void start(Stage primaryStage) {
 		
+		VBox settingsLayout = new VBox(20);
 	}
 }
