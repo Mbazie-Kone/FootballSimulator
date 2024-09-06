765f1c8304cd83f0aebc1b1dd6cd988f5f3b3298
M'bazie Kone <mbazie89@gmail.com>
2024-09-06 02:13:52 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/game/SettingsMenu.java
+++ src/main/java/it/mbaziekone/footballsimulator/game/SettingsMenu.java
@@ -1,6 +1,8 @@
 package it.mbaziekone.footballsimulator.game;
 
 import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -10,5 +12,9 @@ public class SettingsMenu {
 		
 		VBox settingsLayout = new VBox(20);
 		settingsLayout.setAlignment(Pos.CENTER);
+		
+		
+		Label volumeLabel = new Label("Music volume:");
+		Slider volumeSlider = new Slider(0, 100, 50); // Min: 0, Max: 100, Default: 50
 	}
 }
