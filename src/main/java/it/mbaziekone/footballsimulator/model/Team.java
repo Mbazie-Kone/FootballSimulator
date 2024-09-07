bc5190e1e84710a61b8d6dc0416ff52779126e36
M'bazie Kone <mbazie89@gmail.com>
2024-09-07 21:21:20 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/model/Team.java
+++ src/main/java/it/mbaziekone/footballsimulator/model/Team.java
@@ -31,37 +31,5 @@ public class Team implements Serializable {
 	
 	@OneToMany(mappedBy = "team")
 	private List<Player> players = new ArrayList<>();
-	
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Nation getNation() {
-		return nation;
-	}
-
-	public void setNation(Nation nation) {
-		this.nation = nation;
-	}
-
-	public List<Player> getPlayers() {
-		return players;
-	}
-
-	public void setPlayers(List<Player> players) {
-		this.players = players;
-	}
 
 }
