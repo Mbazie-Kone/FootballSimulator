07f11d387d5fb403cce60303404220a806c49936
M'bazie Kone <mbazie89@gmail.com>
2024-09-07 11:37:17 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/game/PhysicalAttribute.java
+++ src/main/java/it/mbaziekone/footballsimulator/game/PhysicalAttribute.java
@@ -2,21 +2,12 @@ package it.mbaziekone.footballsimulator.game;
 
 public enum PhysicalAttribute {
 	
-	SPEED("Speed"),
-    ACCELERATION("Acceleration"),
-    AGILITY("Agility"),
-    BALANCE("Balance"),
-    STRENGTH("Strength"),
-    STAMINA("Stamina"),
-    FITNESS("Fitness");
+	SPEED,
+    ACCELERATION,
+    AGILITY,
+    BALANCE,
+    STRENGTH,
+    STAMINA,
+    FITNESS;
 
-    private final String attributeName;
-
-    PhysicalAttribute(String attributeName) {
-        this.attributeName = attributeName;
-    }
-
-    public String getAttributeName() {
-        return attributeName;
-    }
 }
