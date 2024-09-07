31b0dc86b1ab9cf94b6a68356f1e4dd1651f1769
M'bazie Kone <mbazie89@gmail.com>
2024-09-07 12:24:46 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/game/PlayerRole.java
+++ src/main/java/it/mbaziekone/footballsimulator/game/PlayerRole.java
@@ -2,23 +2,13 @@ package it.mbaziekone.footballsimulator.game;
 
 public enum PlayerRole {
 	
-	GOALKEEPER("Goalkeeper"),
-    FULL_BACK("Full Back"),
-    CENTER_BACK("Center Back"),
-    DEFENSIVE_MIDFIELDER("Defensive Midfielder"),
-    CENTRAL_MIDFIELDER("Central Midfielder"),
-    WINGER("Winger"),
-    ATTACKING_MIDFIELDER("Attacking Midfielder"),
-    SECOND_STRIKER("Second Striker"),
-    CENTER_FORWARD("Center Forward");
-
-    private final String roleName;
-
-    PlayerRole(String roleName) {
-        this.roleName = roleName;
-    }
-
-    public String getRoleName() {
-        return roleName;
-    }
+	GOALKEEPER,
+    FULL_BACK,
+    CENTER_BACK,
+    DEFENSIVE_MIDFIELDER,
+    CENTRAL_MIDFIELDER,
+    WINGER,
+    ATTACKING_MIDFIELDER,
+    SECOND_STRIKER,
+    CENTER_FORWARD;
 }
