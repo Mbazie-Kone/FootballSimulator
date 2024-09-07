fd371b7a04208bbb5a0d1dbea80514ee29b71eb7
M'bazie Kone <mbazie89@gmail.com>
2024-09-07 11:33:03 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/game/MentalAttribute.java
+++ src/main/java/it/mbaziekone/footballsimulator/game/MentalAttribute.java
@@ -2,25 +2,15 @@ package it.mbaziekone.footballsimulator.game;
 
 public enum MentalAttribute {
 	
-	AGGRESSIVENESS("Aggressiveness"),
-    VISION("Vision"),
-    CREATIVITY("Creativity"),
-    DETERMINATION("Determination"),
-    WORK_RATE("Work Rate"),
-    COURAGE("Courage"),
-    OFF_THE_BALL("Off The Ball"),
-    POSITIONING("Positioning"),
-    DECISION_MAKING("Decision Making"),
-    COMPOSURE("Composure"),
-	CONCENTRATION("Concentration");
-
-    private final String attributeName;
-
-    MentalAttribute(String attributeName) {
-        this.attributeName = attributeName;
-    }
-
-    public String getAttributeName() {
-        return attributeName;
-    }
+	AGGRESSIVENESS,
+    VISION,
+    CREATIVITY,
+    DETERMINATION,
+    WORK_RATE,
+    COURAGE,
+    OFF_THE_BALL,
+    POSITIONING,
+    DECISION_MAKING,
+    COMPOSURE,
+	CONCENTRATION;
 }
