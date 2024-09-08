9caf336ebd20679e351305ca4ac4f0089cfd1e77
M'bazie Kone <mbazie89@gmail.com>
2024-09-09 01:30:21 +0200

    Updates

--- src/main/java/it/mbaziekone/footballsimulator/game/MainUI.java
+++ src/main/java/it/mbaziekone/footballsimulator/game/MainUI.java
@@ -19,7 +19,7 @@ public class MainUI extends Application {
 	public void start(Stage primaryStage) {
 		
 		//
-		String videoPath = "file:/videos/DemoIntro.mp4";
+		String videoPath = getClass().getResource("/videos/DemoIntro.mp4").toExternalForm();
 		
 		// 
 		Media media = new Media(videoPath);
@@ -28,40 +28,7 @@ public class MainUI extends Application {
 		//
 		MediaView mediaView = new MediaView(mediaPlayer);
 		
-		// game logo
-		Image logo = new Image("file:logo.png"); // "Insert the path to the logo"
-		ImageView logoView = new ImageView(logo);
-		logoView.setFitWidth(300);
-		logoView.setFitHeight(300);
 		
-		// game name
-		Label gameTitle = new Label("Football Simulator");
-		gameTitle.setStyle("-fx-font-size: 36px; -fx-font-weight: bold; -fx-text-fill: white;");
-		
-		// Added logo and title to the StackPane
-		splashLayout.getChildren().addAll(logoView, gameTitle);
-		
-		// Creation of the scene
-		Scene splashScene = new Scene(splashLayout, 800, 600);
-		
-		// Setting the background color (black in this case)
-		splashLayout.setStyle("-fx-background-color: black;");
-		
-		// Fade-in/fade-out animation
-		FadeTransition fadeIn = new FadeTransition(Duration.seconds(20), splashLayout);
-		fadeIn.setFromValue(0.0);
-		fadeIn.setToValue(1.0);
-		fadeIn.setOnFinished(e -> {
-			// After the fade-in animation, load the main menu
-			showMainMenu(primaryStage);
-		});
-		
-		// Display the startup scene
-		primaryStage.setScene(splashScene);
-		primaryStage.show();
-		
-		// Start the animation
-		fadeIn.play();
 			
 	}
 	
